f62584934a2e772ecc5a1fedbc2d2bb4c6d18575
Willians Nieves <williansnieves@gmail.com>
2025-05-17 23:04:37 +0200

    fix: correct heading tags in Bio component for semantic HTML

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -2,10 +2,10 @@ export function Bio() {
   return (
     <>
       <div className="flex flex-col gap-1 items-center">
-        <h1 className="text-white text-2xl font-semiboldç">Jessica Randall</h1>
-        <h3 className="text-[#C4F82A] text-sm font-bold">
+        <h1 className="text-white text-2xl font-semibold">Jessica Randall</h1>
+        <h2 className="text-[#C4F82A] text-sm font-bold">
           London, United Kingdom
-        </h3>
+        </h2>
       </div>
 
       <p className="text-white text-sm font-extralight">
